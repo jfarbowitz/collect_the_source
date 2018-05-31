a68700933c9f174591dbd426456125a57b625de6
Jonathan Farbowitz <jfarbowitz@gmail.com>
2018-05-31 12:22:36 -0500

    Initial commit

---
+++ collect_source.js
@@ -0,0 +1,11 @@
+// JavaScript Loop for Time-based Media Conservators
+
+function collectTheSourceCode() {
+	alert("Collect the Source Code!");
+}
+
+var you = "tbm_conservator";
+
+while (you = "tbm_conservator") {
+	collectTheSourceCode();
+}
